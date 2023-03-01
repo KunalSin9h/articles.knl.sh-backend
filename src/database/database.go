70f9a1a52a68ef1c337f240716bd858826e3ebec
Kunal Singh <kunalsin9h@gmail.com>
2023-03-01 21:01:43 +0530

    add more enpoints viz get-artices-meta & get-article-meta

--- src/database/database.go
+++ src/database/database.go
@@ -12,6 +12,10 @@ type Article struct {
 	Id, Title, Slug, Description, Date, Md string
 }
 
+type ArticleMeta struct {
+	Id, Title, Slug, Description, Date string
+}
+
 func CreateDB(driver string, source string) *sql.DB {
 	db, err := sql.Open(driver, source)
 	if err != nil {
@@ -32,7 +36,7 @@ func InsertArticle(db *sql.DB, title, slug, description, date, md string) {
 func GetArticles(db *sql.DB) []Article {
 	row, err := db.Query("select * from articles")
 	if err != nil {
-		log.Fatal(err.Error())
+		return []Article{}
 	}
 
 	var articles []Article
@@ -52,4 +56,33 @@ func GetSingleArticle(slug string, db *sql.DB) Article {
 	row.Scan(&art.Id, &art.Title, &art.Slug, &art.Description, &art.Date, &art.Md)
 
 	return art
+}
+
+func GetArticlesMeta(db *sql.DB) []ArticleMeta {
+	row, err := db.Query("select id, title, slug, description, date from articles")
+
+	if err != nil {
+		return []ArticleMeta{}
+	}
+
+	var artMeta []ArticleMeta
+
+	for row.Next() {
+		meta := ArticleMeta{}
+		row.Scan(&meta.Id, &meta.Title, &meta.Slug, &meta.Description, &meta.Date)
+
+		artMeta = append(artMeta, meta)
+	}
+
+	return artMeta
+}
+
+func GetSingleArticleMeta(slug string, db *sql.DB) ArticleMeta {
+	row := db.QueryRow("select id, title, slug, description, date from articles where slug = ?", slug)
+
+	var artMeta = ArticleMeta{}
+
+	row.Scan(&artMeta.Id, &artMeta.Title, &artMeta.Slug, &artMeta.Description, &artMeta.Date)
+
+	return artMeta
 }
